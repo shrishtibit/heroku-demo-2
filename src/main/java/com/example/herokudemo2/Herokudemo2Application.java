cf90ba2eb118d0c1fc499db883c926ccc21017aa
Shrishti Kumari <shristikumarigomia@gmail.com>
2021-07-08 19:06:59 +0530

    heroku deploy

---
+++ src/main/java/com/example/herokudemo2/Herokudemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.herokudemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Herokudemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Herokudemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/herokudemo2/Herokudemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokudemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Herokudemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
